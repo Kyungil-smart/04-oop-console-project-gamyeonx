6795ece34de28f69e63f207602115e99077e0e00
Geunhyung Song <gamyeonx@gmail.com>
2026-01-13 00:38:37 +0900

    feat: fix BFS, DFS
    2차 배열로 쓸 수 있게 바꿈.

--- Chocobo's Mysterious Dungeon/Monsters/MonsterBFS.cs
+++ Chocobo's Mysterious Dungeon/Monsters/MonsterBFS.cs
@@ -1,33 +1,50 @@
-﻿//using System;
-//using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
+class Solution
+{
+    public int solution(int[,] maps)
+    {
+        int answer = 0;
+        int maxY = maps.GetLength(0);
+        int maxX = maps.GetLength(1);
 
-//public class MonsterBFS
-//{
-//    public List<int> BFS(int start)
-//    {
-//        List<int> path = new List<int>();
-//        Queue<int> queue = new Queue<int>();
+        int[] dy = { -1, 1, 0, 0 };
+        int[] dx = { 0, 0, -1, 1 };
 
-//        bool[] visited = new bool[_vertextCount];
-//        visited[start] = true;
-//        queue.Enqueue(start);
+        int[,] moveCount = new int[maxY, maxX];
 
-//        while (queue.Count > 0)
-//        {
-//            int current = queue.Dequeue();
-//            path.Add(current);
+        Queue<(int, int)> queue = new Queue<(int, int)>();
+        queue.Enqueue((0, 0));
+        moveCount[0, 0] = 1;
 
-//            for (int i = 0; i < _vertextCount; i++)
-//            {
-//                if (_matrix[current, i] && !visited[i])
-//                {
-//                    visited[i] = true;
-//                    queue.Enqueue(i);
-//                }
-//            }
-//        }
-//        return path;
-//    }
-//}
+        while (queue.Count > 0)
+        {
+            (int Y, int X) current = queue.Dequeue();
+            int currentY = current.Y;
+            int currentX = current.X;
 
+            if (currentY == maxY - 1 && currentX == maxX - 1)
+            {
+                return answer = moveCount[currentY, currentX];
+            }
+
+            for (int i = 0; i < 4; i++)
+            {
+                int targetY = currentY + dy[i];
+                int targetX = currentX + dx[i];
+
+                if (targetY >= 0 && targetY < maxY && targetX >= 0 && targetX < maxX)
+                {
+                    if (maps[targetY, targetX] == 1 && moveCount[targetY, targetX] == 0)
+                    {
+                        moveCount[targetY, targetX] = moveCount[currentY, currentX] + 1;
+                        queue.Enqueue((targetY, targetX));
+                    }
+                }
+            }
+        }
+
+        return answer = -1;
+    }
+}
--- Chocobo's Mysterious Dungeon/Monsters/MonsterDFS.cs
+++ Chocobo's Mysterious Dungeon/Monsters/MonsterDFS.cs
@@ -1,33 +1,50 @@
-﻿//using System;
-//using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
+class Solution
+{
+    public int solution(int[,] maps)
+    {
+        int answer = 0;
+        int maxY = maps.GetLength(0);
+        int maxX = maps.GetLength(1);
 
-//public class MonsterDFS
-//{
-//    public List<int> DFS(int start)
-//    {
-//        List<int> path = new List<int>();
-//        Stack<int> stack = new Stack<int>();
+        int[] dy = { -1, 1, 0, 0 };
+        int[] dx = { 0, 0, -1, 1 };
 
-//        bool[] visited = new bool[_vertextCount];
-//        visited[start] = true;
-//        stack.Push(start);
+        int[,] moveCount = new int[maxY, maxX];
 
-//        while (stack.Count > 0)
-//        {
-//            int current = stack.Pop();
-//            path.Add(current);
+        Stack<(int, int)> stack = new Stack<(int, int)>();
+        stack.Push((0, 0));
+        moveCount[0, 0] = 1;
 
-//            for (int i = 0; i < _vertextCount; i++)
-//            {
-//                if (_matrix[current, i] && !visited[i])
-//                {
-//                    visited[i] = true;
-//                    stack.Push(i);
-//                }
-//            }
-//        }
-//        return path;
-//    }
-//}
+        while (stack.Count > 0)
+        {
+            (int Y, int X) current = stack.Pop();
+            int currentY = current.Y;
+            int currentX = current.X;
 
+            if (currentY == maxY - 1 && currentX == maxX - 1)
+            {
+                return answer = moveCount[currentY, currentX];
+            }
+
+            for (int i = 0; i < 4; i++)
+            {
+                int targetY = currentY + dy[i];
+                int targetX = currentX + dx[i];
+
+                if (targetY >= 0 && targetY < maxY && targetX >= 0 && targetX < maxX)
+                {
+                    if (maps[targetY, targetX] == 1 && moveCount[targetY, targetX] == 0)
+                    {
+                        moveCount[targetY, targetX] = moveCount[currentY, currentX] + 1;
+                        stack.Push((targetY, targetX));
+                    }
+                }
+            }
+        }
+
+        return answer = -1;
+    }
+}
